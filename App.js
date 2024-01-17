79b0f2ec2cc267f799b7ea7e22603b2b8c6ef4b4
AdibFirmansyah <adibfirmansyah27@gmail.com>
2024-01-17 21:55:54 +0700

    Layout

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { View, ScrollView } from "react-native";
 import SampleComponent from "./SampleComponent";
 import StylingReact from "./StylingReact";
+import Flexbox from "./Flexbox";
 
 const App = () => {
   return (
     <View>
       <ScrollView>
-        <SampleComponent />
-        <StylingReact />;
+        {/* <SampleComponent />
+        <StylingReact /> */}
+        <Flexbox />
       </ScrollView>
     </View>
   );
---
+++ Flexbox.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Text, View, Image } from "react-native";
+import github from "./github.png";
+
+class Flexbox extends Component {
+  render() {
+    return (
+      <View>
+        <View style={{ flexDirection: "row", backgroundColor: "#c8d6e5", alignItems: "center", justifyContent: "space-between" }}>
+          <View style={{ backgroundColor: "#ee5253", width: 50, height: 50 }}></View>
+          <View style={{ backgroundColor: "#feca57", width: 50, height: 50 }}></View>
+          <View style={{ backgroundColor: "#1dd1a1", width: 50, height: 50 }}></View>
+          <View style={{ backgroundColor: "#5f27cd", width: 50, height: 50 }}></View>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <Image source={github} style={{ width: 50, height: 50, borderRadius: 30 }} />
+          <View style={{ justifyContent: "center", marginLeft: 10 }}>
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>Muhammad Adib Firmansyah</Text>
+            <Text>Full Stack Developer</Text>
+          </View>
+        </View>
+        <View style={{ flexDirection: "row", justifyContent: "space-around", marginTop: 20 }}>
+          <Text>Home</Text>
+          <Text>About</Text>
+          <Text>Project</Text>
+          <Text>Setting</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default Flexbox;
