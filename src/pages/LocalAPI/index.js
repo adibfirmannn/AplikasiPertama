c8224fba85e6896a75bdefb6eabb6f20320cf000
AdibFirmansyah <adibfirmansyah27@gmail.com>
2024-01-25 23:32:29 +0700

    membuat create ke local API

--- src/pages/LocalAPI/index.js
+++ src/pages/LocalAPI/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View, Image } from "react-native";
 
 const Item = () => {
@@ -16,14 +17,31 @@ const Item = () => {
 };
 
 const LocalAPI = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [bidang, setBidang] = useState("");
+  const submit = () => {
+    const data = {
+      name,
+      email,
+      bidang,
+    };
+    // console.log("data before send", data);
+    axios.post("http://192.168.43.237:3000/users", data).then((res) => {
+      console.log("res ", res);
+      setName("");
+      setEmail("");
+      setBidang("");
+    });
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.textTitle}>Local API JSON Server</Text>
       <Text>Masukkan Anggota Adib Coding</Text>
-      <TextInput placeholder="Nama Lengkap" style={styles.input} />
-      <TextInput placeholder="Email" style={styles.input} />
-      <TextInput placeholder="Bidang" style={styles.input} />
-      <Button title="Simpan" />
+      <TextInput placeholder="Nama Lengkap" style={styles.input} value={name} onChangeText={(value) => setName(value)} />
+      <TextInput placeholder="Email" style={styles.input} value={email} onChangeText={(value) => setEmail(value)} />
+      <TextInput placeholder="Bidang" style={styles.input} value={bidang} onChangeText={(value) => setBidang(value)} />
+      <Button title="Simpan" onPress={submit} />
       <View style={styles.line} />
       <Item />
       <Item />
